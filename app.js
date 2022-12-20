be8eaffcfa2405d581f6afac440590614747d460
Kenton Kim <109005186+KentonKim@users.noreply.github.com>
2022-12-19 19:24:38 -0800

    added text when selecting non-fight options from menu

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const audioButton = document.querySelector('#audio-button')
 const ppBox = document.querySelector("#pp-main")
 const menuBox = document.querySelector("#menu-main")
 const emptyBox = document.querySelector(".empty")
-const textBox   = document.querySelector('.chat-inner')
+const textBox   = document.querySelector('#typing')
 const ppType = document.querySelector('#move-type')
 const ppCount = document.querySelector('#pp-count')
 let menuState = 1
 
+const dialogueList = 
+    ["What will USER do?",
+    "You don't have any more Pokemon to use!",
+    'Your bag is empty!',
+    "Can't escape!"]
 const menuTL = menuOptions[0] // Fight / Rock
 const menuBL = menuOptions[1] // Pokemon / Scissor
 const menuTR = menuOptions[2] // Bag / Paper
@@ -24,64 +29,66 @@ let ppBL= 15
 let ppBR= 5
 
 document.addEventListener('keyup',moveMenu)
-document.addEventListener('keyup',exitMenu)
+document.addEventListener('keyup',pressingAB)
 menuTL.classList.add(select)
 
 function moveMenu(e) {
-    switch(e.key) {
-        case 'w' : // up 
-            if (menuBL.classList.contains(select)) {
-                menuTL.classList.add(select)
-                menuBL.classList.remove(select)
-                playButton()
-            }
-            if (menuBR.classList.contains(select)) {
-                menuTR.classList.add(select)
-                menuBR.classList.remove(select)
-                playButton()
-            }
-            ppUpdate()
-            break
-        case 'a' : // left
-            if (menuTR.classList.contains(select)) {
-                menuTL.classList.add(select)
-                menuTR.classList.remove(select)
-                playButton()
-            }
-            if (menuBR.classList.contains(select)) {
-                menuBL.classList.add(select)
-                menuBR.classList.remove(select)
-                playButton()
-            }
-            ppUpdate()
-            break
-        case 's' : // down
-            if (menuTL.classList.contains(select)) {
-                menuBL.classList.add(select)
-                menuTL.classList.remove(select)
-                playButton()
-            }
-            if (menuTR.classList.contains(select)) {
-                menuBR.classList.add(select)
-                menuTR.classList.remove(select)
-                playButton()
-            }
-            ppUpdate()
-            break
-        case 'd' : // right
-            if (menuTL.classList.contains(select)) {
-                menuTR.classList.add(select)
-                menuTL.classList.remove(select)
-                playButton()
-            }
-            if (menuBL.classList.contains(select)) {
-                menuBR.classList.add(select)
-                menuBL.classList.remove(select)
-                playButton()
-            }
-            ppUpdate()
-            break
-    } 
+    if (menuState != 0){
+        switch(e.key) {
+            case 'w' : // up 
+                if (menuBL.classList.contains(select)) {
+                    menuTL.classList.add(select)
+                    menuBL.classList.remove(select)
+                    playButton()
+                }
+                if (menuBR.classList.contains(select)) {
+                    menuTR.classList.add(select)
+                    menuBR.classList.remove(select)
+                    playButton()
+                }
+                ppUpdate()
+                break
+            case 'a' : // left
+                if (menuTR.classList.contains(select)) {
+                    menuTL.classList.add(select)
+                    menuTR.classList.remove(select)
+                    playButton()
+                }
+                if (menuBR.classList.contains(select)) {
+                    menuBL.classList.add(select)
+                    menuBR.classList.remove(select)
+                    playButton()
+                }
+                ppUpdate()
+                break
+            case 's' : // down
+                if (menuTL.classList.contains(select)) {
+                    menuBL.classList.add(select)
+                    menuTL.classList.remove(select)
+                    playButton()
+                }
+                if (menuTR.classList.contains(select)) {
+                    menuBR.classList.add(select)
+                    menuTR.classList.remove(select)
+                    playButton()
+                }
+                ppUpdate()
+                break
+            case 'd' : // right
+                if (menuTL.classList.contains(select)) {
+                    menuTR.classList.add(select)
+                    menuTL.classList.remove(select)
+                    playButton()
+                }
+                if (menuBL.classList.contains(select)) {
+                    menuBR.classList.add(select)
+                    menuBL.classList.remove(select)
+                    playButton()
+                }
+                ppUpdate()
+                break
+        } 
+    }
 }
 
 function ppUpdate() {
@@ -111,10 +118,11 @@ function playButton() {
     audioButton.play()
 }
 
-function exitMenu(e) {
+function pressingAB(e) {
     // Selecting from menu
     if (menuState == 1 && e.key == "p") {
         if (menuTL.classList.contains(select)) {
+            menuState = 2 // attack menu
             ppBox.classList.remove("hidden")
             emptyBox.classList.add("hidden")
             menuBox.style.flex = "3"
@@ -123,14 +131,21 @@ function exitMenu(e) {
             menuBL.innerHTML = 'SCISSORS'
             menuBR.innerHTML = 'CHARGE'
             playButton()
-            menuState = 2 // attack menu
             ppUpdate()
         }
         else {
+            menuState = 0
             menuBox.classList.add("hidden")
             playButton()
-            menuState = 0
+            menuDialogue()
         }
+    }
+
+    // Going back to menu from non-fight optiono
+    else if (menuState == 0 && e.key == 'p') {
+        menuState = 1
+        menuDialogue()
+        menuBox.classList.remove('hidden')
     }
 
     // Selecting an Attack
@@ -151,4 +166,22 @@ function exitMenu(e) {
         playButton()
     }
     
+}
+
+function menuDialogue() {
+    if (menuState == 0) {
+        if (menuOptions[1].classList.contains(select)) {
+            textBox.innerHTML = dialogueList[1]
+        }
+        else if (menuOptions[2].classList.contains(select)) {
+            textBox.innerHTML = dialogueList[2]
+        }
+        else if (menuOptions[3].classList.contains(select)) {
+            textBox.innerHTML = dialogueList[3]
+        }
+    }
+
+    if (menuState == 1) {
+        textBox.innerHTML = dialogueList[0]
+    }
 }
